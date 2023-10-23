ca5e854b80e5dc624fb4f8ad4e126e950c22d478
Alperen Bozkurt <78342156+Hadacar@users.noreply.github.com>
2023-10-23 12:53:22 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import {
   createBrowserRouter,
 } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './utils/http.js';
+
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
+import EditEvent, {
+  loader as editEventLoader,
+  action as editEventAction,
+} from './components/Events/EditEvent.jsx';
+import { queryClient } from './utils/http.js';
 
 const router = createBrowserRouter([
   {
@@ -18,7 +22,6 @@ const router = createBrowserRouter([
   {
     path: '/events',
     element: <Events />,
-
     children: [
       {
         path: '/events/new',
@@ -33,18 +36,19 @@ const router = createBrowserRouter([
       {
         path: '/events/:id/edit',
         element: <EditEvent />,
+        loader: editEventLoader,
+        action: editEventAction
       },
     ],
   },
 ]);
 
-
 function App() {
   return (
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />;
-
-  </QueryClientProvider>)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
